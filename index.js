6cf131499f734b03b271ca43efa2d16442bb2336
Paul Riverain <paulriverain@yahoo.com>
2019-01-04 18:56:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
 //let dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-
 function dwarfRollCall(dwarves) {
 let dwarfList = []
-for (let i = 0; i < dwarves.length; i++){
-  dwarfList.push([i+1]+ '. '+ dwarves[i])
+let count = 0
+for (let i = 0; i<dwarves.length; i++){
+  count+=1
+  dwarfList.push(/*[1+i]*/count+'. '+dwarves[i])
 }
-return dwarfList.join(' ') + " "
+return dwarfList.join(' ')+' '
 }
+
 
 //let planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
-
 function summonCaptainPlanet(planeteerCalls){
-let planet = [];
+let calls = []
 let i = 0;
-while (i < planeteerCalls.length){
-  planet.push(planeteerCalls[i].toUpperCase() + '!');
+while(i<planeteerCalls.length){
+  calls.push(planeteerCalls[i].toUpperCase()+'!')
   i++;
 }
-return planet
+return calls
 }
 
 //let words = ["hello", "bye", "sky", "birdy"]
 function longPlaneteerCalls(words) {
-  var word = false
-  for (let i = 0; i < words.length; i++){
+let word = false
+for (let i=0; i<words.length; i++){
   if (words[i].length > 4){
-  return word = true
+    return word = true
+  }
 }
 return word
 }
+
+//let foods = ['bread','milk', 'eggs', 'camembert', 'butter']
+function findTheCheese(foods){
+let cheese = ['cheddar', 'gouda', 'camembert']
+for (let i=0; i<cheese.length; i++){
+var isCheese = foods.includes(cheese[i])
+if (isCheese === true){
+  return cheese[i]
+}
+}
+return 'no cheese!'
 }
 
-//let foods = ["tomato soup", "oyster crackers", "gouda"]
-function findTheCheese (foods) {
-  let basket = ['cheddar', 'gouda', 'camembert']
-  for (let i = 0; i < basket.length; i++){
-  var cheese1 = foods.includes(basket[i])
-if (cheese1 === true){
-    return basket[i]
+//let foods = ['bread','milk', 'eggs', 'camembert', 'butter']
+/*function findTheCheese(foods){
+let cheese = ['cheddar', 'gouda', 'camembert']
+for (let i=0; i<cheese.length; i++){
+  for (let j=0; j<foods.length; j++){
+    if (cheese[i] === foods[j]){
+      return cheese[i]
+    }
   }
 }
-  return 'no cheese!'
-  }
+return 'no cheese!'
+}*/
